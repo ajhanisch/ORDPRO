316a8fec0d841a7c31919347d126227de5ae7952
Ashton Hanisch <ajhanisch@gmail.com>
2017-10-24 02:13:08 +0000

    Upload New File

---
+++ Testing-Functions/Archive-Directory.ps1
@@ -0,0 +1,22 @@
+$source = "C:\temp\ORDPRO\TMP\LOGS\2017-10-23_04-43-42"
+$destination = "C:\temp\ORDPRO\TMP\LOGS\2017-10-23_04-43-42_archive.zip"
+
+function Archive-Directory
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)][String]$source,
+        [Parameter(Mandatory=$true)][string]$destination
+    )
+
+    if(Test-path $destination) 
+    {
+        Remove-item $destination
+    }
+
+    Add-Type -assembly "system.io.compression.filesystem"
+
+    [io.compression.zipfile]::CreateFromDirectory($Source, $destination) 
+}
+
+Archive-Directory -source $($source) -destination $($destination)
