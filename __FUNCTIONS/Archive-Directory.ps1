c0af0625ffbc4690abb8d9626f0c97c0c6db5c61
Ashton Hanisch <ajhanisch@gmail.com>
2017-10-27 12:54:25 +0000

    Upload New File

---
+++ __FUNCTIONS/Archive-Directory.ps1
@@ -0,0 +1,19 @@
+﻿function Archive-Directory
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)][String]$source,
+        [Parameter(Mandatory=$true)][string]$destination
+    )
+
+    if(Test-path $destination) 
+    {
+        Remove-item $destination
+    }
+
+    Write-Log -level [INFO] -log_file $($log_file) -message "Zipping up $($source) to $($destination) now."
+    Write-Verbose "Zipping up $($source) to $($destination) now."
+
+    Add-Type -assembly "system.io.compression.filesystem"
+    [io.compression.zipfile]::CreateFromDirectory($source, $destination)
+}
