d1b3fdc350b585372058644272d27b694049f010
Ashton Hanisch <ajhanisch@gmail.com>
2017-10-31 08:13:41 +0000

    Replace Undo-PreviousSessions.ps1

--- __FUNCTIONS/Undo-PreviousSessions.ps1
+++ __FUNCTIONS/Undo-PreviousSessions.ps1
@@ -100,21 +100,32 @@
         $formats.Set_Item("F_1020A",$format_1020A)   
 
         # Populate array to hold current files
-        $files = Get-ChildItem -Path "C:\temp\OUTPUT\UICS" -Exclude "__PERMISSIONS" -Recurse -Include "*.txt" | Select Directory, Name
-        $dir_file = @{}
+        $files_uics = Get-ChildItem -Path "$($uics_directory_output)" -Exclude "__PERMISSIONS" -Recurse -Include "*.txt" | Select Directory, Name
+        $files_ord_managers = Get-ChildItem -Path "$($ordmanagers_orders_by_soldier_output)" -Exclude "__PERMISSIONS" -Recurse -Include "*.txt" | Select Directory, Name
+        $dir_file_uics = @{}
+        $dir_file_ord_managers = @{}
 
         # Populate hash table to hold directory and file name of files created prviously that wish to be removed.
-        foreach($f in $files)
+        foreach($f in $files_uics)
         {
             # If hash table doesn't contain key of $($f.Name), add the file name and directory to hash table.
-            if(!($dir_file.ContainsKey($($f.Name))))
+            if(!($dir_file_uics.ContainsKey($($f.Name))))
             {
-                $($dir_file.Add($($f.Name), $($f.Directory)))
+                $($dir_file_uics.Add($($f.Name), $($f.Directory)))
+            }
+        }
+
+        foreach($f in $files_ord_managers)
+        {
+            # If hash table doesn't contain key of $($f.Name), add the file name and directory to hash table.
+            if(!($dir_file_ord_managers.ContainsKey($($f.Name))))
+            {
+                $($dir_file_ord_managers.Add($($f.Name), $($f.Directory)))
             }
         }
 
         # Search each format array for files already created, remove them if found, add success and failure results to csv.
-        foreach($f in $dir_file.GetEnumerator())
+        foreach($f in $dir_file_uics.GetEnumerator())
         {
             $file_name = $f.Key
             $file_directory = $f.Value
@@ -131,7 +142,72 @@
                 {
                     Write-Log -log_file $log_file -message "Found $($file_name) in $($format_type). Removing $($file_directory)\$($file_name)."
                     Write-Verbose "Found $($file_name) in $($format_type). Removing $($file_directory)\$($file_name)."
-                    # Remove file
+
+                    Remove-Item -Path "$($file_directory)\$($file_name)" -Force
+                    if($?)
+                    {
+                        Write-Log -log_file $log_file -message "$($file_name) removed from $($file_directory) successfully."
+                        Write-Verbose "$($file_name) removed from $($file_directory) successfully."
+                    }
+
+                    $hash = @{
+                        FILE = $($file_name);
+                        DIRECTORY = $($file_directory);
+                        FORMAT = $($format_type);
+                        STATUS = 'REMOVED';
+                    }
+
+                    $file_removed = New-Object -TypeName PSObject -Property $hash
+                    $files_removed += $file_removed
+
+                    #Write-Log -log_file $log_file -message "Files Removed: $($files_removed.Count)."
+                    #Write-Verbose "Files Removed: $($files_removed.Count)."
+                }
+                else
+                {
+                    #Write-Log -log_file $log_file -message "$($file_name) not found in $($format_type). Do not remove."
+                    #Write-Verbose "$($file_name) not found in $($format_type). Do not remove."
+                    $files_not_removed += $($file_name)
+
+                    <#
+                    $hash = @{
+                        FILE = $($file_name);
+                        DIRECTORY = $($file_directory);
+                        FORMAT = $($format_type);
+                        STATUS = 'NOT REMOVED';
+                    }
+
+                    $file_not_removed = New-Object -TypeName PSObject -Property $hash
+                    $files_not_removed += $file_not_removed
+                    #>
+                }
+            }
+        }
+
+        foreach($f in $dir_file_ord_managers.GetEnumerator())
+        {
+            $file_name = $f.Key
+            $file_directory = $f.Value
+
+            foreach($kvp in $formats.GetEnumerator())
+            {
+                # Call function to allow for keyboard commands during function runtime.
+                Process-KeyboardCommands -sw $($sw)
+
+                $format_type = $kvp.Key
+                $format_value = $kvp.Value
+
+                if($($format_value) -contains $($file_name) -and $($files_removed) -notcontains $($file_name))
+                {
+                    Write-Log -log_file $log_file -message "Found $($file_name) in $($format_type). Removing $($file_directory)\$($file_name)."
+                    Write-Verbose "Found $($file_name) in $($format_type). Removing $($file_directory)\$($file_name)."
+
+                    Remove-Item -Path "$($file_directory)\$($file_name)" -Force
+                    if($?)
+                    {
+                        Write-Log -log_file $log_file -message "$($file_name) removed from $($file_directory) successfully."
+                        Write-Verbose "$($file_name) removed from $($file_directory) successfully."
+                    }
 
                     $hash = @{
                         FILE = $($file_name);
