e8aa5ceb9663607125b729b243b4f2378205167d
Ashton Hanisch <ajhanisch@gmail.com>
2017-11-30 07:08:41 -0700

    Update maint script
    Remove redundant array use and switched order or processing to be faster and make more sense. Moved writing of all output until the end after removal numbers presenting

--- UIC-Maintenance.ps1
+++ UIC-Maintenance.ps1
@@ -19,36 +19,15 @@ $active_txt = "$($current_directory)\$($run_date)_active.txt"
 $inactive_txt = "$($current_directory)\$($run_date)_inactive.txt"
 $the_rest_txt = "$($current_directory)\$($run_date)_the_rest.txt"
 
-<#
-ARRAYS
-#>
-$results = @()
-
 Write-Host "Gathering directories and files now."
 #$directories = @(Get-ChildItem -Path $($file_path) -Recurse -Include "*.doc" | ? { $_.FullName -notmatch 'ZZUNK' } | Select Directory, Name)
 $directories = @(Get-ChildItem -Path $($file_path) -Recurse -Include "*.doc" | Select Directory, Name)
 Write-Host "Finished gathering directories and files."
 
-Write-Host "Writing $($directories_names_csv) now."
-$($directories) | Select Directory, Name | Export-Csv -Path $($directories_names_csv) -NoTypeInformation -Force
-Write-Host "Finished writing $($directories_names_csv)."
-
-Write-Host "Populating results array."
-$input_csv = Import-Csv $($directories_names_csv)
-foreach($i in $input_csv)
-{
-    $order = New-Object -TypeName PSObject
-    $order | Add-Member -MemberType NoteProperty -Name Directory -Value $($i.Directory)
-    $order | Add-Member -MemberType NoteProperty -Name File -Value $($i.Name)
-    
-    $results += $order
-}
-Write-Host "Finished populating results array."
-
 Write-Host "Determining ACTIVE or INACTIVE."
-$active = $results | ? { $_.File -match ($time_line -join "|") } | Select -ExpandProperty Directory
-$inactive = $results | ? { $active -notcontains $_.Directory }
-$the_rest = $results | ? { $active -notcontains $_ -and $inactive -notcontains $_ }
+$active = $directories | ? { $_.File -match ($time_line -join "|") } | Select -ExpandProperty Directory
+$inactive = $directories | ? { $active -notcontains $_.Directory }
+$the_rest = $directories | ? { $active -notcontains $_ -and $inactive -notcontains $_ }
 Write-Host "Finished determining ACTIVE or INACTIVE."
 
 Write-Host "Presenting numbers."
@@ -60,15 +39,16 @@ Write-Host "Total: $($results.Directory.Count)"
 Write-Host "----------------------------"
 Write-Host "Finished presenting numbers."
 
-Write-Host "Writing results to $($current_directory) now."
-$active | Out-File $($active_txt)
-$inactive | Out-File $($inactive_txt)
-$the_rest | Out-File $($the_rest_txt)
-Write-Host "Finished writing results to $($current_directory)."
-
 Write-Host "Removing INACTIVE."
 foreach($i in $inactive)
 {
     Remove-Item -Path $($i.Directory) -Force -Verbose
 }
 Write-Host "Finished removing INACTIVE."
+
+Write-Host "Writing results to $($current_directory) now."
+$active | Out-File $($active_txt)
+$inactive | Out-File $($inactive_txt)
+$the_rest | Out-File $($the_rest_txt)
+$($directories) | Select Directory, Name | Export-Csv -Path $($directories_names_csv) -NoTypeInformation -Force
+Write-Host "Finished writing results to $($current_directory)."
